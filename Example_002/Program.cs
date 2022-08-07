8f5015157e17e5b673dab67b826bdf081c41645b
Olga <miarik@yandex.ru>
2022-08-07 07:51:20 -0400

    Example_4

---
+++ Example_002/Program.cs
@@ -0,0 +1,14 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {4,6,7,8,5,3,2,6,8,16};
+
+//int result = Max(
+    
+
+Console.WriteLine(result);
--- Example_003/Program.cs
+++ Example_003/Program.cs
@@ -1,6 +1,11 @@
-﻿void FillArray(int[] collection)
-{
-    int lenght = collection.Length;
-}
-
-int[] array = new int[10];
+﻿int[] array = {1,5,6,7,8,3,6,8,4,6,4,8};
+int n = array.Length;
+int find = 4;
+int index = 0;
+ while (index<n) {
+    if(array[index] == find) {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+ }
